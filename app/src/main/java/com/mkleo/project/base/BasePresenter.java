4b4155b1b7156387a748a0146395e2034fa0b37f
MkSuihan <Mkleo627586724@outlook.com>
2019-08-01 09:39:42 +0800

    修复一个内存泄漏问题

--- app/src/main/java/com/mkleo/project/base/BasePresenter.java
+++ app/src/main/java/com/mkleo/project/base/BasePresenter.java
@@ -1,5 +1,10 @@
 package com.mkleo.project.base;
 
+import java.lang.ref.WeakReference;
+
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+
 /**
  * des:
  * by: Mk.leo
@@ -7,16 +12,48 @@ package com.mkleo.project.base;
  */
 public class BasePresenter<V extends IView> implements IPresenter<V> {
 
-    protected V mView;
+    /* 弱引用,防止内存泄漏 */
+    private WeakReference<V> mViewReference;
+    private CompositeDisposable mCompositeDisposable;
 
     @Override
     public void attachView(V view) {
-        this.mView = view;
+        this.mViewReference = new WeakReference<>(view);
+    }
+
+    /**
+     * 得到View实例
+     *
+     * @return
+     */
+    protected V getView() {
+        return mViewReference.get();
+    }
+
+    /***
+     * 主要防止内存泄漏(比如activity已经finish,但是耗时操作正在进行,
+     * 导致view被持有,所以需要在view被回收前将所有正在执行的操作停止)
+     *
+     * @param disposable
+     */
+    protected void addDisposable(Disposable disposable) {
+        if (null == mCompositeDisposable)
+            mCompositeDisposable = new CompositeDisposable();
+        mCompositeDisposable.add(disposable);
     }
 
     @Override
     public void detachView() {
-        this.mView = null;
+        //停止所有操作
+        if (null != mCompositeDisposable) {
+            mCompositeDisposable.clear();
+            mCompositeDisposable = null;
+        }
+        //清除软引用
+        if (null != mViewReference) {
+            mViewReference.clear();
+            mViewReference = null;
+        }
     }
 
 }
