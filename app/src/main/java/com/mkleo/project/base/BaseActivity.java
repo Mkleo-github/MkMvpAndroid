5e7a6c93ddaa69e0885f4e558ee4f8ba211c499d
MkSuihan <Mkleo627586724@outlook.com>
2020-04-27 17:13:58 +0800

    1.分级化权限请求

--- app/src/main/java/com/mkleo/project/base/BaseActivity.java
+++ app/src/main/java/com/mkleo/project/base/BaseActivity.java
@@ -1,8 +1,6 @@
 package com.mkleo.project.base;
 
 import android.app.Activity;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.CallSuper;
 import android.support.annotation.Nullable;
@@ -12,9 +10,6 @@ import com.mkleo.project.app.App;
 import com.mkleo.project.model.eventbus.Eventer;
 import com.mkleo.project.model.eventbus.IEvent;
 import com.mkleo.project.model.eventbus.IEventReceiver;
-import com.mkleo.project.model.permissions.AppSettingsDialog;
-import com.mkleo.project.model.permissions.PermissionCallback;
-import com.mkleo.project.model.permissions.Permissions;
 import com.mkleo.project.utils.MkLog;
 
 import butterknife.ButterKnife;
@@ -23,14 +18,12 @@ import butterknife.Unbinder;
 /**
  * 无presenter的activity
  */
-public abstract class BaseActivity extends AppCompatActivity implements IView, IEventReceiver {
+public abstract class BaseActivity extends PermissionActivity implements IView, IEventReceiver {
 
     //butterknife
     protected Activity mAcitivty;
     private Unbinder mUnbinder;
     private UIKit mUIKit;
-    private String[] mRequestPermissions;
-    private Permissions mPermissions;
 
     @Override
     public final UIKit getUIKit() {
@@ -48,11 +41,9 @@ public abstract class BaseActivity extends AppCompatActivity implements IView, I
         setContentView(getLayoutResId());
         mAcitivty = this;
         mUIKit = new UIKit(this);
-        mRequestPermissions = getRequestPermissions();
         onActivityCreate();
         onActivityReady();
         //获取权限
-        requestPermissions();
         Eventer.getDefault().register(getClass(), this);
     }
 
@@ -63,21 +54,6 @@ public abstract class BaseActivity extends AppCompatActivity implements IView, I
         Eventer.getDefault().unregister(getClass(), this);
         onActivityRelease();
         onActivityDestroy();
-    }
-
-    @CallSuper
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == AppSettingsDialog.DEFAULT_SETTINGS_REQ_CODE) {
-            //从设置界面返回
-            //检测所有权限是否全部获取
-            if (mPermissions.isPermisssionsGranted()) {
-                onAllPermissionsGranted();
-            } else {
-                onSomePermissionsDenied();
-            }
-        }
     }
 
     /**
@@ -108,62 +84,6 @@ public abstract class BaseActivity extends AppCompatActivity implements IView, I
     @Override
     public void onEvent(IEvent event) {
 
-    }
-
-    /**
-     * 获取请求的权限
-     *
-     * @return
-     */
-    protected String[] getRequestPermissions() {
-        return null;
-    }
-
-    /**
-     * 请求权限
-     */
-    protected void requestPermissions() {
-        if (null != mRequestPermissions && mRequestPermissions.length > 0) {
-            mPermissions = new Permissions(this, mRequestPermissions)
-                    .setPermissionCallback(new PermissionCallback() {
-                        @Override
-                        public void onPermissionsGranted() {
-                            onAllPermissionsGranted();
-                        }
-
-                        @Override
-                        public void onSomePermissionsDeniedOrNeverAsk() {
-                            mPermissions.requestPermisssion();
-                        }
-
-                        @Override
-                        public void onSomePermissionsNerverAsk() {
-                            mPermissions.showAppSettingsDialog(new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    //用户取消操作
-                                    printLog("[取消手动授予权限操作]");
-                                    onSomePermissionsDenied();
-                                }
-                            });
-                        }
-                    });
-            mPermissions.requestPermisssion();
-        }
-    }
-
-    /**
-     * 所有权限已经授予
-     */
-    protected void onAllPermissionsGranted() {
-        printLog("[所有权限已经授予]");
-    }
-
-    /**
-     * 部分权限没有授予
-     */
-    protected void onSomePermissionsDenied() {
-        printLog("[部分权限没有授予]");
     }
 
     /**
