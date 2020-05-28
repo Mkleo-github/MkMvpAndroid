9e63c0fa3e076fdab64fdad9cb3e899d365ab3f9
MkSuihan <Mkleo627586724@outlook.com>
2020-05-28 13:42:07 +0800

    1.更新权限管理

---
+++ app/src/main/java/com/mkleo/project/base/IPermissonInterface.java
@@ -0,0 +1,10 @@
+package com.mkleo.project.base;
+
+public interface IPermissonInterface {
+
+    String[] getRequestPermissions();
+
+    void onAllPermissionsGranted();
+
+    void onSomePermissionsDenied();
+}
--- app/src/main/java/com/mkleo/project/base/PermissionActivity.java
+++ app/src/main/java/com/mkleo/project/base/PermissionActivity.java
@@ -1,98 +1,33 @@
 package com.mkleo.project.base;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.CallSuper;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-import com.mkleo.project.model.permissions.AppSettingsDialog;
-import com.mkleo.project.model.permissions.PermissionCallback;
-import com.mkleo.project.model.permissions.Permissions;
 import com.mkleo.project.utils.MkLog;
 
 public class PermissionActivity extends AppCompatActivity {
 
-    private Permissions mPermissions;
-    private String[] mRequestPermissions;
+    private PermissionImp mPermissionImp;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mRequestPermissions = getRequestPermissions();
-        requestPermissions();
+        mPermissionImp = new PermissionImp(getPermissionInterface());
+        mPermissionImp.requestPermissions(this);
     }
 
     @CallSuper
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == AppSettingsDialog.DEFAULT_SETTINGS_REQ_CODE) {
-            //从设置界面返回
-            //检测所有权限是否全部获取
-            if (mPermissions.isPermisssionsGranted()) {
-                onAllPermissionsGranted();
-            } else {
-                onSomePermissionsDenied();
-            }
-        }
+        mPermissionImp.onActivityResult(requestCode);
     }
 
-    /**
-     * 获取请求的权限
-     *
-     * @return
-     */
-    protected String[] getRequestPermissions() {
+    protected IPermissonInterface getPermissionInterface() {
         return null;
-    }
-
-    /**
-     * 请求权限
-     */
-    protected void requestPermissions() {
-        if (null != mRequestPermissions && mRequestPermissions.length > 0) {
-            mPermissions = new Permissions(this, mRequestPermissions)
-                    .setPermissionCallback(new PermissionCallback() {
-                        @Override
-                        public void onPermissionsGranted() {
-                            onAllPermissionsGranted();
-                        }
-
-                        @Override
-                        public void onSomePermissionsDeniedOrNeverAsk() {
-                            mPermissions.requestPermisssion();
-                        }
-
-                        @Override
-                        public void onSomePermissionsNerverAsk() {
-                            mPermissions.showAppSettingsDialog(new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    //用户取消操作
-                                    printLog("[取消手动授予权限操作]");
-                                    onSomePermissionsDenied();
-                                }
-                            });
-                        }
-                    });
-            mPermissions.requestPermisssion();
-        }
-    }
-
-    /**
-     * 所有权限已经授予
-     */
-    protected void onAllPermissionsGranted() {
-        printLog("[所有权限已经授予]");
-    }
-
-    /**
-     * 部分权限没有授予
-     */
-    protected void onSomePermissionsDenied() {
-        printLog("[部分权限没有授予]");
     }
 
     /**
@@ -103,4 +38,5 @@ public class PermissionActivity extends AppCompatActivity {
     protected void printLog(String log) {
         MkLog.print(getClass().getSimpleName(), log);
     }
+
 }
